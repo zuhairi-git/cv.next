5f2bb4692e4bb37132a8c168eb58aa766dff0a3f
Ali Al-Zuhairi <zohairi@live.com>
2023-07-23 22:10:13 +0300

    fixing

--- src/App.js
+++ src/App.js
@@ -1,137 +1,178 @@
-import { Route, Routes, useLocation } from "react-router-dom";
-import { HelmetProvider, Helmet } from "react-helmet-async";
-import Home from "./pages/Home";
-import Portfolio from "./pages/Portfolio";
-import Jobseeking from "./portfolio/Jobseeking";
-import Collaboration from "./portfolio/Collaboration";
-import Blog from "./pages/Blog";
-import Blog01 from "./blogs/Blog01";
-import Blog02 from "./blogs/Blog02";
-import Blog03 from "./blogs/Blog03";
-import Blog04 from "./blogs/Blog04";
+import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { useRoutes, useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga';
-import withTracker from './generic/withTracker';
+
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Jobseeking from './portfolio/Jobseeking';
+import Collaboration from './portfolio/Collaboration';
+import Blog from './pages/Blog';
+import Blog01 from './blogs/Blog01';
+import Blog02 from './blogs/Blog02';
+import Blog03 from './blogs/Blog03';
+import Blog04 from './blogs/Blog04';
 
 ReactGA.initialize('G-QR6LZ8YSLJ');
+
+const routes = [
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/portfolio',
+    element: <Portfolio />
+  },
+  {
+    path: '/portfolio/jobseeking',
+    element: <Jobseeking />
+  },
+  {
+    path: '/portfolio/collaboration',
+    element: <Collaboration />
+  },
+  {
+    path: '/blog',
+    element: <Blog />
+  },
+  {
+    path: '/blogs/circle-of-rhythm',
+    element: <Blog01 />
+  },
+  {
+    path: '/blogs/ios-notifications',
+    element: <Blog02 />
+  },
+  {
+    path: '/blogs/premitive-human',
+    element: <Blog03 />
+  },
+  {
+    path: '/blogs/claude-ai',
+    element: <Blog04 />
+  }
+];
 
 function App() {
   const location = useLocation();
 
-  const getMetaTags = () => {
+  const getMetaTags = (pathname) => {
     const publicUrl = process.env.PUBLIC_URL;
 
-    console.log(publicUrl); // Add this line to log the value of publicUrl
-
     const getPageName = (pathname) => {
-      const parts = pathname.split("/").filter(Boolean);
+      const parts = pathname.split('/').filter(Boolean);
       return parts[parts.length - 1];
     };
 
     switch (location.pathname) {
-      case "/":
+      case '/':
         return {
-          title: "CV Lanes | Webpage",
-          description: "Professional Product/UI/UX Designer",
-          ogTitle: "CV Lanes | Landing Page",
-          ogDescription: "Professional Product Designer",
-          ogType: "website",
+          title: 'CV Lanes | Webpage',
+          description: 'Professional Product/UI/UX Designer',
+          ogTitle: 'CV Lanes | Landing Page',
+          ogDescription: 'Professional Product Designer',
+          ogType: 'website',
           ogImage: 'https://centerux.com/images/sites/corescene.png',
-          ogImageWidth: "1200",
-          ogImageHeight: "630",
-          twitterCreator: "Ali Zuhairi",
-          twitterCard: "My CV and Skills",
+          ogImageWidth: '1200',
+          ogImageHeight: '630',
+          twitterCreator: 'Ali Zuhairi',
+          twitterCard: 'My CV and Skills',
+          ogUrl: window.location.href
         };
-      case "/portfolio":
+      case '/portfolio':
         return {
-          title: "Portfolio",
-          description: "Where Magic Happens",
-          ogType: "Page",
+          title: 'Portfolio',
+          description: 'Where Magic Happens',
+          ogType: 'Page',
           ogImage: `${publicUrl}/img/covers/portfolio-default-image.jpg`,
-          ogImageWidth: "1200",
-          ogImageHeight: "630",
-          imageAlt: "Portfolio",
+          ogImageWidth: '1200',
+          ogImageHeight: '630',
+          imageAlt: 'Portfolio',
+          ogUrl: window.location.href
         };
-      case "/blog":
+      case '/blog':
         return {
-          title: "Blog",
-          description: "To Blog... or not to Blog",
-          ogType: "Page",
+          title: 'Blog',
+          description: 'To Blog... or not to Blog',
+          ogType: 'Page',
           ogImage: `${publicUrl}/img/covers/blog-default-image.jpg`,
-          ogImageWidth: "1200",
-          ogImageHeight: "630",
-          imageAlt: "Blog",
+          ogImageWidth: '1200',
+          ogImageHeight: '630',
+          imageAlt: 'Blog',
+          ogUrl: window.location.href
         };
-      case "/portfolio/jobseeking":
+      case '/portfolio/jobseeking':
         return {
-          title: "Job Seeking Application",
-          description: "Job Seeking Application",
-          ogType: "Portfolio",
+          title: 'Job Seeking Application',
+          description: 'Job Seeking Application',
+          ogType: 'Portfolio',
           ogImage: `${publicUrl}/img/covers/portfolio-jobseeking-default-image.jpg`,
-          ogImageWidth: "1200",
-          ogImageHeight: "630",
-          imageAlt: "Job Seeking Application",
+          ogImageWidth: '1200',
+          ogImageHeight: '630',
+          imageAlt: 'Job Seeking Application',
+          ogUrl: window.location.href
         };
-
-      case "/portfolio/collaboration":
+      case '/portfolio/collaboration':
         return {
-          title: "Collaboration Workflow Platform",
-          description: "Collaboration Workflow Platform",
-          ogType: "Portfolio",
+          title: 'Collaboration Workflow Platform',
+          description: 'Collaboration Workflow Platform',
+          ogType: 'Portfolio',
           ogImage: `${publicUrl}/img/covers/portfolio-collaboration-default-image.jpg`,
-          ogImageWidth: "1200",
-          ogImageHeight: "630",
-          imageAlt: "Collaboration Workflow Platform",
+          ogImageWidth: '1200',
+          ogImageHeight: '630',
+          imageAlt: 'Collaboration Workflow Platform',
+          ogUrl: window.location.href
         };
-
-      case "/blogs/circle-of-rhythm":
+      case '/blogs/circle-of-rhythm':
         return {
-          title: "Daily Rhythm",
-          description: "Circle of Daily Rhythm",
-          ogType: "article",
+          title: 'Daily Rhythm',
+          description: 'Circle of Daily Rhythm',
+          ogType: 'article',
           ogImage: `${publicUrl}/img/covers/blogs-circle-of-rhythm-default-image.jpg`,
-          ogImageWidth: "1200",
-          ogImageHeight: "630",
-          imageAlt: "Daily Rhythm",
+          ogImageWidth: '1200',
+          ogImageHeight: '630',
+          imageAlt: 'Daily Rhythm',
+          ogUrl: window.location.href
         };
-
-      case "/blogs/ios-notifications":
+      case '/blogs/ios-notifications':
         return {
-          title: "Notifications on iOS",
-          description: "How to get notifications on iOS with personal focus on?",
-          ogType: "article",
+          title: 'Notifications on iOS',
+          description: 'How to get notifications on iOS with personal focus on?',
+          ogType: 'article',
           ogImage: `${publicUrl}/img/covers/blogs-ios-notifications-default-image.jpg`,
-          ogImageWidth: "1200",
-          ogImageHeight: "630",
-          imageAlt: "Notifications on iOS",
+          ogImageWidth: '1200',
+          ogImageHeight: '630',
+          imageAlt: 'Notifications on iOS',
+          ogUrl: window.location.href
         };
-
-      case "/blogs/premitive-human":
+      case '/blogs/premitive-human':
         return {
-          title: "Embracing the Era of AI",
+          title: 'Embracing the Era of AI',
           description: "Humanity's Journey to Unparalleled Sophistication",
-          ogType: "article",
+          ogType: 'article',
           ogImage: `${publicUrl}/img/covers/blogs-premitive-human-default-image.jpg`,
-          ogImageWidth: "1200",
-          ogImageHeight: "630",
-          imageAlt: "Era of AI",
+          ogImageWidth: '1200',
+          ogImageHeight: '630',
+          imageAlt: 'Era of AI',
+          ogUrl: window.location.href
         };
-
       default:
         return {
-          title: "CV Lanes | Landing Page",
-          description: ".Professional Product Designer",
-          ogTitle: "CV Lanes | Landing Page",
-          ogDescription: "..Professional Product Designer",
-          ogImage: `${publicUrl}/img/covers/${getPageName(
-            location.pathname
-          )}-default-image.jpg`,
-          ogImageWidth: "1200",
-          ogImageHeight: "630",
+          title: 'CV Lanes | Landing Page',
+          description: '.Professional Product Designer',
+          ogTitle: 'CV Lanes | Landing Page',
+          ogDescription: '..Professional Product Designer',
+          ogImage: `${publicUrl}/img/covers/${getPageName(location.pathname)}-default-image.jpg`,
+          ogImageWidth: '1200',
+          ogImageHeight: '630',
+          ogUrl: window.location.href
         };
     }
   };
 
-  const metaTags = getMetaTags();
+  const metaTags = getMetaTags(location.pathname);
+  const element = useRoutes(routes);
 
   return (
     <HelmetProvider>
@@ -150,28 +191,16 @@ function App() {
             <meta name="twitter:title" content={metaTags.title} />
             <meta name="twitter:description" content={metaTags.description} />
             <meta name="twitter:image" content={metaTags.ogImage} />
-            <meta name="twitter:card" content={metaTags.ogImage} />
+            <meta name="twitter:card" content="summary_large_image" />
           </>
         )}
         {metaTags.imageAlt && (
           <meta property="image:alt" content={metaTags.imageAlt} />
         )}
-        {/* Add other meta tags based on the metaTags object */}
       </Helmet>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/portfolio/jobseeking" element={<Jobseeking />} />
-        <Route path="/portfolio/collaboration" element={<Collaboration />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blogs/circle-of-rhythm" element={<Blog01 />} />
-        <Route path="/blogs/ios-notifications" element={<Blog02 />} />
-        <Route path="/blogs/premitive-human" element={<Blog03 />} />
-        <Route path="/blogs/claude-ai" element={<Blog04 />} />      </Routes>
-
+      {element} {/* Render the element without wrapping it in <BrowserRouter> */}
     </HelmetProvider>
   );
 }
 
-export default withTracker(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom'; // <BrowserRouter> is imported here
 import App from './App';
 
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
 
+root.render(
+  <BrowserRouter> {/* App component is wrapped with <BrowserRouter> */}
+    <App />
+  </BrowserRouter>  
+);
