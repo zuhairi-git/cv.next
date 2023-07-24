e13e2e03073f66bb458f98494594360e33547668
Ali Al-Zuhairi <zohairi@live.com>
2023-07-24 10:39:41 +0300

    Update Portfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,38 +4,48 @@ import Footer from './Footer';
 import { Container, Row, Col } from 'react-bootstrap';
 import PortfolioList from './PortfolioList';
 import BackToTopButton from '../generic/BackToTopButton';
+import { Helmet, HelmetProvider } from 'react-helmet-async'; 
 
 const PortfolioPage = () => {
     return (
-
-        <div className='portfolio-bg'>
-
-            <TopNavGeneric />
-            <div className="portfolio" id="portfolio">
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-sm-12 col-md-6">
-                            <div className="hero-content">
-                                <div className="hero-text text-start wow fadeInUp" data-wow-delay="0.2s">
-                                    <h1 className='text-light mt-lg-5 mt-md-3 mt-sm-2 mt-xs-2 text-shadow primary-title'>Where Magic Happens</h1>
+        <HelmetProvider>
+            <Helmet>
+                <title>Portfolio</title>
+                <meta name="description" content="Where Magic Happens" />
+                <meta property="og:type" content="Portfolio Page" />
+                <meta property="og:url" content="https://next.cvslanes.com/portfolio" />
+                <meta property="og:image" content="https://cdn.cvlanes.com/images/covers/portfolio-collaboration-default-image.jpg" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+            </Helmet>
+            <div className='portfolio-bg'>
+                <TopNavGeneric />
+                <div className="portfolio" id="portfolio">
+                    <div className="container">
+                        <div className="row align-items-center">
+                            <div className="col-sm-12 col-md-6">
+                                <div className="hero-content">
+                                    <div className="hero-text text-start wow fadeInUp" data-wow-delay="0.2s">
+                                        <h1 className='text-light mt-lg-5 mt-md-3 mt-sm-2 mt-xs-2 text-shadow primary-title'>Where Magic Happens</h1>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1 className="text-center my-5 primary-title display-2">My Portfolio</h1>
+                        </Col>
+                    </Row>
+                    <PortfolioList />
+                </Container>
+                <BackToTopButton />
+                <Footer />
             </div>
-            <Container>
-                <Row>
-                    <Col>
-                        <h1 className="text-center my-5 primary-title display-2">My Portfolio</h1>
-                    </Col>
-                </Row>
-                <PortfolioList />
-            </Container>
-            <BackToTopButton />
-            <Footer />
-
-        </div>
+        </HelmetProvider>
     );
 };
+
 export default PortfolioPage;
