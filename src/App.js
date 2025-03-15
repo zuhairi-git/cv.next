d3a9cbbc3438af74382dd205da64787b87d502d0
Ali Al-Zuhairi <ali.zuh.fin@gmail.com>
2025-03-15 18:09:18 +0200

    add meta

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { Helmet } from "react-helmet"; // Import Helmet
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Jobseeking from "./portfolio/Jobseeking";
@@ -9,26 +10,37 @@ import Blog02 from "./blogs/Blog02";
 import Blog03 from "./blogs/Blog03";
 import Blog04 from "./blogs/Blog04";
 import Blog05 from "./blogs/Blog05";
-import BlogTest from "./blogs/BlogTest";
+import BlogTest from "./blogs/BlogTest"; // Updated blog name
 
 function App() {
-
-
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/portfolio/jobseeking" element={<Jobseeking />} />
-      <Route path="/portfolio/collaboration" element={<Collaboration />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/blogs/circle-of-rhythm" element={<Blog01 />} />
-      <Route path="/blogs/ios-notifications" element={<Blog02 />} />
-      <Route path="/blogs/premitive-human" element={<Blog03 />} />
-      <Route path="/blogs/claude-ai" element={<Blog04 />} />
-      <Route path="/blogs/hiring-process" element={<Blog05 />} />
-      <Route path="/blogs/blog-test" element={<BlogTest />} />
-      
-    </Routes>
+    <>
+      <Helmet>
+        <title>My Blog - Insights and Stories</title>
+        <meta name="description" content="Welcome to My Blog, where we share insights and stories on various topics." />
+        <meta property="og:site_name" content="My Blog" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="My Blog - Insights and Stories" />
+        <meta property="og:description" content="Explore insightful articles and stories on various topics." />
+        <meta property="og:image" content="http://localhost:3000/static/media/ai04.511e2f5d5e913df6680c.jpg" />
+        <meta property="og:url" content="https://cvlanes.com/" />
+      </Helmet>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/portfolio/jobseeking" element={<Jobseeking />} />
+        <Route path="/portfolio/collaboration" element={<Collaboration />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blogs/circle-of-rhythm" element={<Blog01 />} />
+        <Route path="/blogs/ios-notifications" element={<Blog02 />} />
+        <Route path="/blogs/premitive-human" element={<Blog03 />} />
+        <Route path="/blogs/claude-ai" element={<Blog04 />} />
+        <Route path="/blogs/hiring-process" element={<Blog05 />} />
+        <Route path="/blogs/blog-test" element={<BlogTest />} />
+
+      </Routes>
+    </>
   );
 }
 
--- src/blogs/BlogTest.js
+++ src/blogs/BlogTest.js
@@ -8,7 +8,7 @@ const BlogTest = () => {
                 <title>Circle of Rhythm - My Blog</title>
                 <meta property="og:title" content="Circle of Rhythm - My Blog" />
                 <meta property="og:description" content="An in-depth look into the rhythm of life and music." />
-                <meta property="og:image" content="https://cvlanes.com/static/media/itsmeai.e60725f64d02549f9001.webp" />
+                <meta property="og:image" content="http://localhost:3000/static/media/ai04.511e2f5d5e913df6680c.jpg" />
                 <meta property="og:url" content="https://cvlanes.com/blogs/blog-test" />
                 <meta property="og:type" content="article" />
             </Helmet>
